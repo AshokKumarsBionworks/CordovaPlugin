af0b06fac7044b7252af49b17634299fe7777981
AshokKumar TS <ashokkumar.ts@bionworks.com>
2020-10-06 13:52:37 +0530

    js function changed

--- www/BionSmartLockPlugin.js
+++ www/BionSmartLockPlugin.js
@@ -1,13 +1,22 @@
-var exec = require('cordova/exec');
+var BionSmartLockPlugin = function(){};
 
-exports.storePassword = function (arg0, success, error) {
-    exec(success, error, 'BionSmartLockPlugin', 'storePassword', [arg0]);
+BionSmartLockPlugin.prototype.storePassword = function(success, failure, args){
+    cordova.exec(success, failure, "BionSmartLockPlugin", "storePassword", [args]);
 };
 
-exports.retrievePassword = function (arg0, success, error) {
-    exec(success, error, 'BionSmartLockPlugin', 'retrievePassword', [arg0]);
+BionSmartLockPlugin.prototype.retrievePassword = function(success, failure, args){
+    cordova.exec(success, failure, "BionSmartLockPlugin", "retrievePassword", [args]);
 };
 
-exports.deletePassword = function (arg0, success, error) {
-    exec(success, error, 'BionSmartLockPlugin', 'deletePassword', [arg0]);
+BionSmartLockPlugin.prototype.deletePassword = function(success, failure, args){
+    cordova.exec(success, failure, "BionSmartLockPlugin", "deletePassword", [args]);
 };
+
+//Plug in to Cordova
+cordova.addConstructor(function() {
+    if (!window.Cordova) {
+        window.Cordova = cordova;
+    };
+    if(!window.plugins) window.plugins = {};
+    window.plugins.BionSmartLockPlugin = new BionSmartLockPlugin();
+});
